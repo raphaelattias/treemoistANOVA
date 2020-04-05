89664a14164a72f9d5a84cd053cd1925da9909c2
vpastore <59581321+vpastore@users.noreply.github.com>
2020-04-05 15:50:52 +0200

    Delete forward backward.R

--- forward backward.R
+++
@@ -1,6 +0,0 @@
-#FORWARD/BACKWARD
-fit.full <- lm(moisture ~ .,data=treemoist.dat)
-for.aic <- step(lm(moisture ~ 1, data = treemoist.dat), direction = "forward", scope = formula(fit.full), k = 2, trace = 0)    
-for.bic <- step(lm(moisture ~ 1, data=treemoist.dat), direction = "forward", scope = formula(fit.full), k = log(32), trace = 0)         # forward BIC
-back.aic <- step(fit.full, direction = "backward", k = 2, trace = 0)        # backward AIC
-back.bic <- step(fit.full, direction = "backward", k = log(32), trace = 0)  # backward BIC
