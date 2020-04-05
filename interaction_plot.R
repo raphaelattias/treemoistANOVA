109911ff9c7adbdd2359e452349b0195dbafc4c9
vpastore <59581321+vpastore@users.noreply.github.com>
2020-04-05 15:50:43 +0200

    Delete interaction_plot.R

--- interaction_plot.R
+++
@@ -1,8 +0,0 @@
-#INTERACTION PLOTS
-layout(matrix(1:2,ncol=1))
-interaction.plot(treemoist.dat$species, treemoist.dat$branches, treemoist.dat$moisture)
-interaction.plot(treemoist.dat$species, treemoist.dat$location, treemoist.dat$moisture)
-interaction.plot(treemoist.dat$species, treemoist.dat$transpiration, treemoist.dat$moisture)
-interaction.plot(treemoist.dat$branches, treemoist.dat$location, treemoist.dat$moisture)
-interaction.plot(treemoist.dat$branches, treemoist.dat$transpiration, treemoist.dat$moisture)
-interaction.plot(treemoist.dat$location, treemoist.dat$transpiration, treemoist.dat$moisture)
